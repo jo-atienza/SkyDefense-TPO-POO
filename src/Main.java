11d0cb9e6d02468a6308e235f7819dd3219d485d
Josefina Atienza <jatienza@uade.edu.ar>
2026-06-02 11:16:01 -0300

    Main provisorio que verifica funcionamiento

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--- Iniciando Sky Defense ---");
+
+        // Al instanciar el controlador principal:
+        // Juego crea a Nivel, Jugador, Escuadron, Avion, etc.
+        Juego juego = new Juego();
+
+        // imorime  algo en la consola para verificar que todo funciona.
+        if (!juego.isJuegoTerminado()) {
+            System.out.println("¡El juego ha sido inicializado correctamente!");
+            System.out.println("Listo para defender el espacio aéreo.");
+        }
+    }
+}
